a731c3369ae40fdbdb3d85fac105901e6a0213cd
Taejoon Park <tpark747949@gmail.com>
2025-07-07 16:39:03 +0900

    I don't even kow.

--- pose-viewer/src/App.js
+++ pose-viewer/src/App.js
@@ -1,4 +1,6 @@
+
 import React from 'react';
+import { Link } from 'react-router-dom';
 import snuhLogo from './assets/snuh-logo.svg'; // Import the logo
 import mediscLogo from './assets/medisc-logo.png'; // Import the Medisc logo
 import './App.css'; // Import your CSS file for styling
@@ -8,16 +10,25 @@ function App() {
     <div className="App">
       {/* Header with transparent navy blue background */}
       <header className="header">
-        <img src={snuhLogo} alt="SNUH Logo" className="logo" />
+        {/* SNUH Logo as a link */}
+        <a href="https://icmit.snuh.org" target="_blank" rel="noopener noreferrer">
+          <img src={snuhLogo} alt="SNUH Logo" className="logo" />
+        </a>
         <div className="divider" />
-        <img src={mediscLogo} alt="Medisc Logo" className="logo" />
+        {/* Medisc Logo as a link */}
+        <a href="https://medisc.org" target="_blank" rel="noopener noreferrer">
+          <img src={mediscLogo} alt="Medisc Logo" className="logo" />
+        </a>
         <div className="divider" />
-        <h1 className="app-title">PoseViewer</h1>
+        {/* App title as a link to home */}
+        <Link to="/" className="app-title-link">
+          <h1 className="app-title">PoseViewer</h1>
+        </Link>
       </header>
 
       <div className="content">
         {/* Main content of the app */}
-        <p>Welcome to PoseViewer!</p>
+        <p>In progress...</p>
       </div>
     </div>
   );
